9f5e34ad8cbb2b897062c3f281d7b5e0a82fcc69
Fan Yiwei <nf140901@gmail.com>
2023-05-01 16:39:35 +0800

    Using fect to send ajax

--- src/Component/Content/index.jsx
+++ src/Component/Content/index.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.css'
 
+const key = '895284fb2d2c50a520ea537456963d9c'
+
+
+const getgeocodingFromAPI = async (city) => {
+  const geocodingUrl = `https://api.openweathermap.org/geo/1.0/direct?q='shanghai'&limit=1&appid=${key}`
+  const cords = await (await fetch(geocodingUrl)).json()
+  return cords[0]
+}
+
+const getDataFromAPI = async () => {
+  const cords = await getgeocodingFromAPI()
+  const { lat, lon } = cords
+  const dataUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
+  const data = await (await fetch(dataUrl)).json()
+  return data
+}
+
 export default function Content() {
+  const [data, setData] = useState({})
+
+  useEffect(() => {
+    getDataFromAPI().then((res) => {
+      setData(res);
+      console.log(res);
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
+
   return (
     <div className='content'>
-      <div className="text">sdaadas</div>
+      <div className="text">{data.lat}</div>
     </div>
   )
 }
--- src/Component/Header/index.jsx
+++ src/Component/Header/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { Connect } from 'react-redux';
 import './index.css'
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 
-export default function Header() {
+function Header() {
+  const [iptLocation, setIptLocation] = useState()
+  const locationRef = useRef(0)
+
+  const handleIpt = () => {
+    const { value } = locationRef.current
+    setIptLocation(value)
+  }
+
+  useEffect(() => {
+    console.log(iptLocation);
+  })
   return (
     <div className='header'>
       <div className="search-box">
-        <input type="text" name="" id="" className='location-input' placeholder='Location...' />
-        <Button className='search-btn' variant="contained" startIcon={<SearchIcon />}>
+        <input type="text" name="" id="" className='location-input' placeholder='Location...' ref={locationRef} />
+        <Button className='search-btn' variant="contained" startIcon={<SearchIcon />} onClick={handleIpt}>
         </Button>
       </div>
     </div>
   )
 }
+
+
+export default Connect()(Header)
